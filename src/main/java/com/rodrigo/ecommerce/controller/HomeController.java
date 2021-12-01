4f4b95e49597b35175c7943a798d27534bd9cd23
Rodrigo Lopez <lopsanrd57@gmail.com>
2021-11-30 19:34:11 -0600

    Agregado vista ver producto al usuario.

--- src/main/java/com/rodrigo/ecommerce/controller/HomeController.java
+++ src/main/java/com/rodrigo/ecommerce/controller/HomeController.java
@@ -1,16 +1,19 @@
 package com.rodrigo.ecommerce.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import com.rodrigo.ecommerce.service.ProductoService;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
+	
+	private final Logger LOG = LoggerFactory.getLogger(HomeController.class);
 	
 	@Autowired
 	private ProductoService productoService;
@@ -19,6 +22,12 @@ public class HomeController {
 	public String home(Model model) {
 		model.addAttribute("productos",productoService.findAll());
 		return "usuario/home";
+	}
+	
+	@GetMapping("productohome/{id}")
+	public String productoHome(@PathVariable Integer id) {
+		LOG.info("Id enviado: {}",id);
+		return "usuario/productohome";
 	}
 
 }
